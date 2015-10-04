964eb49be48ba4ffe18b07583c7330c40c80d367
SiyingZhang <siyingzhang@icloud.com>
2015-10-04 17:27:41 -0400

    Modified ratings python file.

--- lab3_ratings.py
+++ lab3_ratings.py
@@ -1,3 +1,8 @@
+###########################################
+### Creates Database
+### Updates Database with Movie Rating Data
+###########################################
+
 import sqlite3 as lite
 import sys
 import os 
@@ -5,33 +10,42 @@ import csv
 import urllib
 import re
 
+#Open the object list in the website and store it in the dict.
 ratingsList = "http://boxnumbertwo.com/MovieData/ratings.list"
 response = urllib.urlopen(ratingsList)
 movies = dict()
-for line in response:
-	words = re.sub(' +',' ',line.strip())
-	
-	line = words.split(" ",3)
-	
-	movies[line[3]]=line[2]
 
-directoryForDB = "~/Desktop/Data_Analytics/"
+for line in response:
+	words = re.sub(' +',' ',line.strip())	
+	line = words.split(" ",3)
+
+	#remove special characters at the beginning of the string
+	wordReplace1 = re.sub('["\$#@&''*!.]','',line[3])
+	#remove "{.*}" behind of the string
+	wordReplace2 = re.sub('{.*}', '', wordReplace1)
+	#store the modified value into dict.
+	movies[wordReplace2]=line[2]
+
+## Set directory to YOUR computer and folder
+directoryForDB = "./MovieData/"
 if not os.path.exists(directoryForDB):
 	os.makedirs(directoryForDB)
 
 directoryForDB = directoryForDB + "movies.db"
 ## If database does not exist, creates items
 ## If database does exist, opens it
-
 con = lite.connect(directoryForDB)
+#use 8-bit strings instead of unicode string
 con.text_factory = str
+
 with con:
 	cur = con.cursor()
-	cur.execute("DROP TABLE IF EXISTS MovieRatings")
-	cur.execute("CREATE TABLE MovieRatings(movie TEXT, rating REAL)")
+	cur.execute("DROP TABLE IF EXISTS ratings") 
+	#Create a table named "ratings", movieName as the primary key, and ratings is another column.
+	cur.execute("CREATE TABLE ratings(movieName TEXT PRIMARY KEY, rating REAL)")
 	for key in movies:
-	    insertStatement = "INSERT INTO MovieRatings VALUES(?,?)"
-	    parms=(key, movies[key])
-	    cur.execute(insertStatement,parms)
+		insertStatement = 'INSERT INTO ratings VALUES(?,?)'
+		parms = (key,movies[key])
+	
+		cur.execute(insertStatement, parms)
 	con.commit()
-
