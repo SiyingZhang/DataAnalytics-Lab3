d9498373dce00ecc2bab543c095dbf5dbd43e181
Ken <xynoci.k@gmail.com>
2015-10-04 21:29:54 -0400

    Remove empty table rows.

--- budget.py
+++ budget.py
@@ -24,52 +24,46 @@ con = lite.connect(directoryForDB)
 con.text_factory = str
 
 with con:
+	count=0
 	cur = con.cursor()
 	cur.execute("DROP TABLE IF EXISTS budget") 
 	#Create a table named "budget", movieName as the primary key, and budget is another column.
 	cur.execute("CREATE TABLE budget(movieName TEXT PRIMARY KEY, budget REAL, domestic REAL, worldwide REAL)")
 
 	for row in soup('table')[0].findAll('tr'):
-		# dateDic=dic()
 		budgetDic = dict()
 		domesticDic=dict()
 		worldwideDic=dict()
 		key = ''
-		for td in row.findAll('td')[2:3]:
-			key+=td.text
+		if len(row.findAll('td')) != 0:
+			for td in row.findAll('td')[2:3]:
+				key+=td.text
 	
-		for td in row.findAll('td')[1:2]:
-			key+=' ('+(str)(td.text)[-4:]+')'	
+			for td in row.findAll('td')[1:2]:
+				key+=' ('+(str)(td.text)[-4:]+')'	
 	
-		for td in row.findAll('td')[3:4]:
-			budgetValue = []
-			budgetValue = td.text.replace(',','')
-			budgetValue=budgetValue.replace('$','')
-			budgetDic[key]=(str)(budgetValue)
+			for td in row.findAll('td')[3:4]:
+				budgetValue = []
+				budgetValue = td.text.replace(',','')
+				budgetValue=budgetValue.replace('$','')
+				budgetDic[key]=(str)(budgetValue)
 	
-		for td in row.findAll('td')[4:5]:
-			domesticValue=[]
-			domesticValue = td.text.replace(',','')
-			domesticValue=domesticValue.replace('$','')
-			domesticDic[key]=(str)(domesticValue)
+			for td in row.findAll('td')[4:5]:
+				domesticValue=[]
+				domesticValue = td.text.replace(',','')
+				domesticValue=domesticValue.replace('$','')
+				domesticDic[key]=(str)(domesticValue)
 	
-		for td in row.findAll('td')[5:6]:
-			worldwideValue=[]
-			worldwideValue = td.text.replace(',','')
-			worldwideValue = worldwideValue.replace('$','')
-			worldwideDic[key] = (str)(worldwideValue)
+			for td in row.findAll('td')[5:6]:
+				worldwideValue=[]
+				worldwideValue = td.text.replace(',','')
+				worldwideValue = worldwideValue.replace('$','')
+				worldwideDic[key] = (str)(worldwideValue)
 			
-		print key
-		# print budgetDic[key]
-		# print domesticDic[key]
-		# print worldwideDic[key]
-		# insertStatement = 'INSERT INTO budget VALUES(?,?,?,?)'
-		# parms = (key,budgetDic[key],domesticDic[key],worldwideDic[key])
-		# cur.execute(insertStatement, parms)
-		# print "INSERT", key
-	# con.commit()
-	
-		# print "\nbudgetDic: ",budgetDic
-		# print "\nworldwideDic: ",worldwideDic
-		# print "\ndomesticDic: ",domesticDic
-	
+			insertStatement = 'INSERT INTO budget VALUES(?,?,?,?)'
+			parms = (key,budgetDic[key],domesticDic[key],worldwideDic[key])
+			cur.execute(insertStatement, parms)
+			print "Inserting: ", key
+			count+=1
+		con.commit()
+	print 'Total records: ',count
