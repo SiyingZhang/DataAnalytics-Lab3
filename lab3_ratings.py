e8c4b83235225809a5e5afa097fc4f18fdd3429a
SiyingZhang <siyingzhang@icloud.com>
2015-10-06 00:17:59 -0400

    added the 'votes' column into the "rating" table.

--- lab3_ratings.py
+++ lab3_ratings.py
@@ -29,7 +29,7 @@ for line in response:
 	# change (2007/I) into (2007) (422303 results / 3572 match)
 	wordReplace4 = re.sub(r'\/.\)',')', wordReplace3)
 	#store the modified value into dict.
-	movies[wordReplace4]=line[2]
+	movies[wordReplace4]={'rating': line[2], 'votes': line[1]}
 
 ## Set directory to YOUR computer and folder
 directoryForDB = "./MovieData/"
@@ -47,10 +47,10 @@ with con:
 	cur = con.cursor()
 	cur.execute("DROP TABLE IF EXISTS ratings") 
 	#Create a table named "ratings", movieName as the primary key, and ratings is another column.
-	cur.execute("CREATE TABLE ratings(movieName TEXT PRIMARY KEY, rating REAL)")
+	cur.execute("CREATE TABLE ratings(movieName TEXT, rating REAL, votes INT)")
 	for key in movies:
-		insertStatement = 'INSERT INTO ratings VALUES(?,?)'
-		parms = (key,movies[key])
+		insertStatement = 'INSERT INTO ratings VALUES(?, ?, ?)'
+		parms = (key, movies[key]['rating'], movies[key]['votes'])
 		count+=1
 		cur.execute(insertStatement, parms)
 		print "Inserting: ", key
